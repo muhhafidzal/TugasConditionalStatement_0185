0ed4a0d18bf65aa6d3e752865b412c14ab0b0d0d
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-03-09 20:02:24 +0700

    menambahkan void inputdata

--- MenghitungBMI.cpp
+++ MenghitungBMI.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+void inputdata(float &berat, float &tinggi){
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
